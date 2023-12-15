116c5d3a130aafc9bd2085f4bca5cca2e0ba741a
sheikh-adib <sheikh.binshabubakar001@stud.fh-dortmund.de>
2023-12-15 20:19:47 +0100

    Update global variable

--- Code/src/class/commonLib.h
+++ Code/src/class/commonLib.h
@@ -17,4 +17,30 @@
 
 using namespace std;
 
+typedef struct
+{
+    float latitudGPS_float;
+    float lontgitudGPS_float;
+}currentGPS_st;
+
+typedef enum
+{
+    sm_init_state,          //0
+    sm_waiting_state,       //1
+    sm_moving_state,        //2
+    sm_aligning_state,      //3
+    sm_stop_state,          //4
+    sm_emergencyStop_state, //5
+    sm_errorHandling_state, //6
+    sm_systemStop_state,    //7
+    sm_leader_state,        //8
+    sm_follower_state,      //9
+}stateMachine_e;
+typedef enum
+{
+    LEADER,
+    FOLLOWER
+}truckRole_e;
+
+
 #endif /* COMMONLIB_H_ */
