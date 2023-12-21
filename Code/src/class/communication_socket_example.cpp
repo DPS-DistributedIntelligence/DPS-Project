9b8d3b76d2c33ce4582ddcb3f73ee91365e8b4c4
Nyuzer <toporovskiy.nikita@gmail.com>
2023-12-21 10:29:36 +0100

    Fixed some mistakes, add truckClass file

--- Code/src/class/communication_socket_example.cpp
+++ Code/src/class/communication_socket_example.cpp
@@ -1,11 +1,15 @@
 // server to which bind clients and write/read messages
 #include <pthread.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
 #include <winsock.h>
-#include <unistd.h>
 
-#define NUM_CONNECTION 3
+
+using namespace std;
+
+#define NUM_CONNECTION 4
 #define PORT 9909
 #define NUM_OF_TRUCKS 4
 
@@ -19,8 +23,17 @@ int opt = 1;
 int address_len = sizeof(srv);
 
 
-pthread_mutex_t mutexNumberOfVisitor = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutexMessage = PTHREAD_MUTEX_INITIALIZER;
 
+
+void writeMessage(string message){
+    pthread_mutex_lock(&mutexMessage);
+    {
+        // message = text 
+        // ...
+    }
+    pthread_mutex_unlock(&mutexMessage);
+}
 
 // connections from trucks
 void* connection(void* threadid)
@@ -39,7 +52,7 @@ void* connection(void* threadid)
     while (true) // here will be functionality of read/write message I think
     {
         valread = read(truck_socket, buffer, 1024);
-        
+
         // here...
 
     }
@@ -99,3 +112,20 @@ int main()
     pthread_exit(NULL);
 
 }
+
+
+/*
+Error occurs - problem with libraries including (library A has to be before B - example)
+
+
+C:/msys64/mingw64/bin/../lib/gcc/x86_64-w64-mingw32/13.1.0/../../../../x86_64-w64-mingw32/bin/ld.exe: C:\Users\topor\AppData\Local\Temp\cc7yf62V.o:communication_socket_example.cpp:(.text+0x50): undefined reference to `__imp_accept'
+C:/msys64/mingw64/bin/../lib/gcc/x86_64-w64-mingw32/13.1.0/../../../../x86_64-w64-mingw32/bin/ld.exe: C:\Users\topor\AppData\Local\Temp\cc7yf62V.o:communication_socket_example.cpp:(.text+0xce): undefined reference to `__imp_WSAStartup'
+C:/msys64/mingw64/bin/../lib/gcc/x86_64-w64-mingw32/13.1.0/../../../../x86_64-w64-mingw32/bin/ld.exe: C:\Users\topor\AppData\Local\Temp\cc7yf62V.o:communication_socket_example.cpp:(.text+0x10e): undefined reference to `__imp_socket'
+C:/msys64/mingw64/bin/../lib/gcc/x86_64-w64-mingw32/13.1.0/../../../../x86_64-w64-mingw32/bin/ld.exe: C:\Users\topor\AppData\Local\Temp\cc7yf62V.o:communication_socket_example.cpp:(.text+0x142): undefined reference to `__imp_setsockopt'
+C:/msys64/mingw64/bin/../lib/gcc/x86_64-w64-mingw32/13.1.0/../../../../x86_64-w64-mingw32/bin/ld.exe: C:\Users\topor\AppData\Local\Temp\cc7yf62V.o:communication_socket_example.cpp:(.text+0x17b): undefined reference to `__imp_htons'
+C:/msys64/mingw64/bin/../lib/gcc/x86_64-w64-mingw32/13.1.0/../../../../x86_64-w64-mingw32/bin/ld.exe: C:\Users\topor\AppData\Local\Temp\cc7yf62V.o:communication_socket_example.cpp:(.text+0x1ad): undefined reference to `__imp_bind'
+C:/msys64/mingw64/bin/../lib/gcc/x86_64-w64-mingw32/13.1.0/../../../../x86_64-w64-mingw32/bin/ld.exe: C:\Users\topor\AppData\Local\Temp\cc7yf62V.o:communication_socket_example.cpp:(.text+0x1e6): undefined reference to `__imp_listen'
+
+
+*/
+
