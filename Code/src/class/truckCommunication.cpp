4dbe9f27b9b2d946e7b19c8985f30f2aaf20e663
Nyuzer <toporovskiy.nikita@gmail.com>
2023-12-21 18:58:04 +0100

    All

--- Code/src/class/truckCommunication.cpp
+++ Code/src/class/truckCommunication.cpp
@@ -1,13 +1,20 @@
-// not sure, but maybe it is possible to do 1 class of truck and make instances?
+// making instances of truck
 #include <iostream>
 #include <unistd.h>
 #include <winsock.h>
+#include <string>
 using namespace std;
  
+// https://www.youtube.com/watch?v=CMDBF84vSRk
+// https://www.youtube.com/watch?v=LtXEMwSG5-8
+
+// playlist below of sockets on C++
+// https://youtu.be/W9b9SaGXIjA?si=yeHrn8aF6ysLnK-X
 
 
 #define PORT 9909
 
+// to connect to communication socket
 
 class truckComm{
     private:
@@ -48,5 +55,39 @@ class truckComm{
             closesocket(truck_socket);
             WSACleanup();
         }
+
+        void sendMessage(const string& message) {
+            // writing functionality should be here?
+            send(truck_socket, message.c_str(), message.length(), 0);
+        }
+
+        void receiveMessages() {
+            // Method to continuously receive messages from the server
+            char buffer[1024] = {0};
+            int valread;
+
+            while (true) {
+
+                // reading functionality should be here?
+
+                valread = recv(truck_socket, buffer, sizeof(buffer), 0);
+                if (valread > 0) {
+                    // Process and handle the received message
+                    cout << "Truck received: " << buffer << std::endl;
+                }
+            }
+        }
 };
 
+int main() {
+    truckComm truck1;
+    truckComm truck2;
+   
+}
+
+
+/*
+same error with libraries import.
+
+
+*/
