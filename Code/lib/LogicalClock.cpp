4ecff49b46bebf51dc695b76f84adee16edbf935
Luis Fernando <luis_rogu@outlook.com>
2024-01-17 22:22:18 +0100

    change data type to int

--- Code/lib/LogicalClock.cpp
+++ Code/lib/LogicalClock.cpp
@@ -1,22 +1,22 @@
-#include "logicalClock.h"
+#include "LogicalClock.h"
 
 void logicalClock::logicalClockTick()
 {
     this->logicalClock_u64++;
 }
 
-bool logicalClock::logicalClockTickCompare(uint64_t varGlobalLogicalClock)
+bool logicalClock::logicalClockTickCompare(int varGlobalLogicalClock)
 {
     return (this->logicalClock_u64 == varGlobalLogicalClock);
 }
 
-void logicalClock::logicalClockUpdate(uint64_t varGlobalLogicalClock)
+void logicalClock::logicalClockUpdate(int varGlobalLogicalClock)
 {
     this->logicalClock_u64 = (this->logicalClock_u64 > varGlobalLogicalClock) ? (this->logicalClock_u64) : (varGlobalLogicalClock);
     //printf(RED_TEXT "Event: Logical clock differs from Leader! \n" RESET_COLOR);
 }
 
-uint64_t logicalClock::get_logicalClock()
+int logicalClock::get_logicalClock()
 {
     return this->logicalClock_u64;
 }
--- Code/lib/LogicalClock.h
+++ Code/lib/LogicalClock.h
@@ -5,7 +5,7 @@
 #include <iostream>
 class logicalClock {
 private:
-    uint64_t logicalClock_u64;
+    int logicalClock_u64;
 public:
     //constructor
     logicalClock();
@@ -20,10 +20,10 @@ public:
     /*
      * Description: Compare 2 logical clocks, the local and a received one, if they are different, return false.
      * Parameters:
-     * 	[in] uint64_t logicalClock
+     * 	[in] int logicalClock
      * 	[out] bool
      */
-    bool logicalClockTickCompare(uint64_t varGlobalLogicalClock);
+    bool logicalClockTickCompare(int varGlobalLogicalClock);
 
     /*
      * Description: Compares to values for the logical clock, current and received one, it updates the current if the
@@ -32,7 +32,7 @@ public:
      * 	[in] null
      * 	[out] null
      */
-    void logicalClockUpdate(uint64_t varGlobalLogicalClock);
+    void logicalClockUpdate(int varGlobalLogicalClock);
 
     /*
      * Description: Return the value of the current clock
@@ -40,7 +40,7 @@ public:
      * 	[in] null
      * 	[out] null
      */
-    uint64_t get_logicalClock();
+    int get_logicalClock();
 };
 
 
