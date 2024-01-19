8691c00f6b1aa77a73b863d980fd6ce7e9b2d57b
sheikh-adib <sheikh.binshabubakar001@stud.fh-dortmund.de>
2024-01-19 22:05:24 +0100

    final version for controller !!!!!

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -6,7 +6,7 @@ int main() {
 
 
     Truck truck1 = Truck(1);
-    truck1.test_leader_exist_controller_behaviour = true;
+    truck1.test_leader_exist_controller_behaviour = false;
     truck1.test_follower_exist_controller_behaviour = false;
     truck1.run();
     return 0;
